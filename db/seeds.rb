efe735b57659223af5065e82697ef645255b2691
taman <taman@tamans-MacBook-Pro.local>
2018-07-14 15:47:08 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
+
+PROJECT_USERS = {
+  "taman@gmail.com" => PROJECT_TITLES[0, 4],
+  "mekky@gmail.com" => PROJECT_TITLES[4..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+  user = User.new(email: email, password:"secret")
+  projects.each{|project| user.projects.build(title: project)}
+  user.save
+end
