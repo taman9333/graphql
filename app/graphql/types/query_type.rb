ad2311d1745a1828e0f0ccd9dc80fd6bb4453a6c
taman <taman@tamans-MacBook-Pro.local>
2018-07-14 17:33:08 +0200

    adding current user & fetching projects of him

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,12 +13,12 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :me, Types::UserType do
     description "The current user"
-    resolve -> (obj, args, ctx) { User.first }
+    resolve -> (obj, args, ctx) { ctx[:current_user] }
   end
 
   field :projects, types[Types::ProjectType] do
     description "the projects for the current user"
-    resolve -> (obj, args, ctx) { Project.all }
+    resolve -> (obj, args, ctx) { ctx[:current_user].projects }
   end
 
 end
